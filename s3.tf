76b054366bc972a309b9fee774b45a9fd40bb186
gbogojevic <goranbogojevic@gmail.com>
2021-12-13 10:08:23 +0100

    test-change-s3

--- s3.tf
+++ s3.tf
@@ -7,12 +7,3 @@ resource "aws_s3_bucket" "b" {
     Environment          = "Dev"
   }
 }
-esource "aws_s3_bucket" "c" {
-  bucket = "my-iac-s3-simple-demo-tf-test-bucket1"
-  acl    = "private"
-
-  tags = {
-    Name                 = "My bucket1"
-    Environment          = "Dev"
-  }
-}
