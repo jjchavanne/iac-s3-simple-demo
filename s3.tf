4e8550b49581051c38b2c6694a5fa5b3a5ad15ac
gbogojevic <goranbogojevic@gmail.com>
2022-03-10 15:24:24 +0100

    adding one more bucket 2

--- s3.tf
+++ s3.tf
@@ -7,12 +7,3 @@ resource "aws_s3_bucket" "b" {
     Environment          = "Dev"
   }
 }
-resource "aws_s3_bucket" "c" {
-  bucket = "my-iac-s3-simple-demo-tf-test-bucket"
-  acl    = "private"
-
-  tags = {
-    Name                 = "My bucket"
-    Environment          = "Dev"
-  }
-}
